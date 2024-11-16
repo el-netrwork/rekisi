9d37ba83423bfc0b5b5fc1e8ef39c709021e47ce
ochiainaoto-g-2 <ochiai@eln.co.jp>
2024-11-16 09:13:49 +0900

    談山神社を作成した

---
+++ tanzan/lib/features/stamp/provider/register_stamp_use_case_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'register_stamp_use_case_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$registerStampUseCaseHash() =>
+    r'f9b276662d1dcc69afe3eee29381efcc8f2e6a44';
+
+/// See also [RegisterStampUseCase].
+@ProviderFor(RegisterStampUseCase)
+final registerStampUseCaseProvider = AutoDisposeNotifierProvider<
+    RegisterStampUseCase, RegisterStampState>.internal(
+  RegisterStampUseCase.new,
+  name: r'registerStampUseCaseProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$registerStampUseCaseHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$RegisterStampUseCase = AutoDisposeNotifier<RegisterStampState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
