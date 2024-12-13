58822ca027a79ff82dd16815783d19b8b5c41d94
shokiogawa <shouki2578@gmail.com>
2024-12-13 23:03:36 +0900

    神宮1追加

---
+++ jingyu-1/lib/app/configuration/production/config_production.dart
@@ -0,0 +1,4 @@
+class ConfigProduction {
+  static const assetHostUrl = "https://jinja-net.jp/stamp-jinguu1";
+  // static const int validMeter = 50;
+}
---
+++ jingyu-1/lib/common/use_case/get_csv_from_asset_use_case.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:csv/csv.dart';
+import 'package:csv/csv_settings_autodetection.dart';
+import 'package:http/http.dart' as http;
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:stamp_rally/common/data/model/csv_model.dart';
+import 'package:stamp_rally/core/flogger.dart';
+
+part 'get_csv_from_asset_use_case.g.dart';
+
+@riverpod
+Future<CsvModel?> getCsvFromAssetUseCase(GetCsvFromAssetUseCaseRef ref, {required String url}) async {
+  try {
+    final response = await http.get(Uri.parse(url));
+    const d = FirstOccurrenceSettingsDetector(eols: ['\r\n', '\n']);
+    final table = const CsvToListConverter(csvSettingsDetector: d).convert(utf8.decode(response.body.runes.toList()));
+    return CsvModel(
+      columnNames: table.first.map((e) => e.toString()).toList(),
+      rows: table.sublist(1),
+    );
+  } catch (e, st) {
+    Flogger.e('[GetCsvAssetUseCase]', error: e, stackTrace: st);
+  }
+  return null;
+}
