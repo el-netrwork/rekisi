ac0e44004c62bc648c67c48407ef7b39c55aacc4
ochiainaoto-g-2 <ochiai@eln.co.jp>
2024-12-14 09:48:32 +0900

    変更

--- jingyu-1/lib/app/configuration/develop/config_develop.dart
+++ jingyu-1/lib/app/configuration/develop/config_develop.dart
@@ -1,4 +1,4 @@
 class ConfigDevelop {
-  static const assetHostUrl = "https://jinja-net.jp/stamp-jinguu7";
+  static const assetHostUrl = "https://jinja-net.jp/stamp-jinguu1";
   // static const int validMeter = 50;
 }
