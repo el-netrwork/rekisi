5e0b32391050af9aa5fb32722c830c377eeb1496
ochiainaoto-g-2 <ochiai@eln.co.jp>
2025-05-06 09:42:20 +0900

    談山神社を作成した

--- asahi0/lib/pages/history/utility/get_font_size_by_length.dart
+++
@@ -1,9 +0,0 @@
-double getFontSizeByLength(String text){
-  // 150文字が上限
-  final double textSize = text.length > 100
-      ? 10
-      : text.length > 60
-      ? 12
-      : 15;
-  return textSize;
-}
--- tanzan-old/android/app/src/main/kotlin/jp/co/eln/stamprally/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package jp.co.eln.stamprally
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity() {}
