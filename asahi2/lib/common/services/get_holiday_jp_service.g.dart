2b210944164bec0157f798384c857a7b0af6fdcd
shokiogawa <shouki2578@gmail.com>
2025-10-05 10:23:41 +0900

    朝日2のSDKverup対応

---
+++ asahi2/lib/common/services/get_holiday_jp_service.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'get_holiday_jp_service.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$getHolidayJpServiceHash() =>
+    r'af8f3a1a7619a2762d71a5e7f72d511cfe517491';
+
+/// See also [getHolidayJpService].
+@ProviderFor(getHolidayJpService)
+final getHolidayJpServiceProvider =
+    AutoDisposeProvider<GetHolidayJpService>.internal(
+  getHolidayJpService,
+  name: r'getHolidayJpServiceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$getHolidayJpServiceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef GetHolidayJpServiceRef = AutoDisposeProviderRef<GetHolidayJpService>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
